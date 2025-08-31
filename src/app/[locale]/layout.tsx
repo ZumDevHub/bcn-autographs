e45a607de47b99f23bab19ae92bd46b430ed1686
ZumDev <contact.zumdev@gmail.com>
2025-08-31 10:07:16 +0200

    responsive 1.2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,15 +5,14 @@ function isLocale(value: string): value is Locale {
   return (locales as readonly string[]).includes(value);
 }
 
-export default async function LocaleLayout({
+export default function LocaleLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  // 👇 fuerza a resolver los params
-  const { locale } = await Promise.resolve(params);
+  const { locale } = params;
 
   if (!isLocale(locale)) {
     notFound();
